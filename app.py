5e27aef4cd6aec0493dc372be054daa85e635324
geopgeopp <geopgeopp@tutamail.com>
2024-12-08 21:29:26 +0100

    add test internet

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-import platform, os
+import platform, os, sys
 from colorama import Fore, Back, Style
+import urllib.request as urec
 
 class app:
     def __init__(self):
         self.system = self.osDetector()
+        self.connection = self.check_connection()
+
+    @staticmethod
+    def check_connection():
+        try:
+            urec.urlopen("https://www.github.com")
+            return True
+        except:
+            return False
 
     @staticmethod        
     def osDetector():
@@ -19,20 +29,28 @@ class app:
         elif system_operation == "Darwin":
             return "Mac"
         else:
-            return "Desconocido"
+            return "Unknown"
         
 
-    def aa(self):
+    def test(self):
+        permittedOS=False
         system=self.system
-        notSystem=["Linux","Windows","Darwin"]
+        connection=self.connection
+        notSystem=["as","Windows","Darwin","Unknown"]
         if system in notSystem:
             print(Fore.RED + "[!]", Fore.RESET + "Your system is not compatible.")
+            sys.exit()
         else:
-            print("WELCOME!")
+            if connection == True:
+                permittedOS=True
+                print(Fore.GREEN + "[√]", Fore.RESET + "The device has internet access.")
+            else:
+                print(Fore.RED + "[!]", Fore.RESET + "Your device does not have internet access.")
+                sys.exit()
 
 
 
 
 if __name__ == "__main__":
     apps=app()
-    apps.aa()
+    apps.test()
